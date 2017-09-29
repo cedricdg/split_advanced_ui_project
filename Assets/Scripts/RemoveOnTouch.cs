9f2af974ba8a22d3efb7725337ddf2db66daf2f3
Cedric Deege <cedric.deege@gmx.de>
2017-09-29 10:24:59 +0200

    Initial Commit including Unity gitignore.

---
+++ Assets/Scripts/RemoveOnTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveOnTouch : MonoBehaviour {
+
+    public string DestroyTag;
+    public Transform InstantiateOnTriggerEnter;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Scoreball")) {
+			Debug.Log("1");
+            Destroy(other);
+            if(InstantiateOnTriggerEnter != null){
+                Debug.Log("2");
+                var copy = Instantiate(InstantiateOnTriggerEnter);
+                copy.gameObject.SetActive(true);
+            }
+        }
+    }
+
+}
