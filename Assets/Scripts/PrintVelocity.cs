542df93e78d2186e33ae23a4216bffa5568513ff
Cedric Deege <cedric.deege@gmx.de>
2017-10-08 20:32:51 +0200

    Track hands with script

--- Assets/Scripts/PrintVelocity.cs
+++ Assets/Scripts/PrintVelocity.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PrintVelocity : MonoBehaviour {
 
+    public double HandRotationOrigin = 90;
+	public double HandRotationThreshhold = 20;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,8 +14,12 @@ public class PrintVelocity : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        var body = this.GetComponent<Rigidbody>();
+		var body = this.GetComponent<Rigidbody>();
+		Debug.Log("Hi! " + body.transform.eulerAngles.z);
         //Debug.Log(string.Format("Velocity: {0}", body.velocity.normalized));
-        Debug.Log(body.rotation);
+        if(body.transform.eulerAngles.z < HandRotationOrigin + HandRotationThreshhold
+           && body.transform.eulerAngles.z > HandRotationOrigin - HandRotationThreshhold){
+            Debug.Log("Counts!");
+        }
 	}
 }
