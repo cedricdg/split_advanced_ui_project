508273eab54f095f69c191be870630d478550327
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-10-18 13:52:09 +0200

    First gyroscope test

---
+++ Assets/LogPhone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogPhone : MonoBehaviour {
+    public Transform yRotationTarget;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Gyro enabled: " + Input.gyro.enabled);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion deviceRotation = Input.gyro.attitude;
+        Debug.Log(deviceRotation);
+        Vector3 newEulerRotation = deviceRotation.eulerAngles;
+        Vector3 oldEulerRotation = transform.rotation.eulerAngles;
+        transform.rotation.eulerAngles.Set(newEulerRotation.x, oldEulerRotation.y, oldEulerRotation.z);
+        Vector3 oldEulerRotationY = yRotationTarget.transform.rotation.eulerAngles;
+        yRotationTarget.transform.rotation.eulerAngles.Set(oldEulerRotationY.x, newEulerRotation.y, oldEulerRotationY.z);
+	}
+}
