4fb26dd08ec3c40ae7522974531bc701ce294535
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-11-04 19:34:47 +0100

    Reformat CalculateYVelcoity.cs

--- Assets/Scripts/CalculateYVelocity.cs
+++ Assets/Scripts/CalculateYVelocity.cs
@@ -1,75 +1,70 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
-namespace LeapmotionProject
+﻿using UnityEngine;
+
+public class CalculateYVelocity : MonoBehaviour
 {
+    public float HandRotationOrigin = 90;
+    public float HandRotationThreshhold = 20;
 
-    public class CalculateYVelocity : MonoBehaviour
+    public float MaxSpeed = 5f;
+    public float MinSpeed = 1f;
+
+    public float BreakForceWrongAngle = 0.08f;
+    public float BreakForceValidAngle = 0.02f;
+    
+    public float YVelocity;
+
+    public void Update()
     {
-
-        public float HandRotationOrigin = 90;
-        public float HandRotationThreshhold = 20;
-
-        public float MaxSpeed = 5f;
-        public float MinSpeed = 1f;
-
-        public float yVel;
-        public int LastHandVelocityRange;
-        public float BreakForceWrongAngle = 0.08f;
-        public float BreakForceValidAngle = 0.02f;
-
-        public void Update()
+        if (IsValidAngle())
         {
-            if (IsValidAngle())
-			{
-				var body = this.GetComponent<Rigidbody>();
-                if(yVel < body.velocity.y){
-                    yVel = Mathf.Abs(body.velocity.y);
-				}
-            }
-		}
-
-        public void FixedUpdate()
-		{
-            if (IsValidAngle())
+            var body = GetComponent<Rigidbody>();
+            if (YVelocity < body.velocity.y)
             {
-                yVel -= BreakForceValidAngle;
-			}
-			else
-			{
-				yVel -= BreakForceWrongAngle;
+                YVelocity = Mathf.Abs(body.velocity.y);
             }
-
-
-			if (yVel < 0f)
-			{
-				yVel = 0f;
-            } else if(yVel > MaxSpeed + MinSpeed){
-                yVel = MaxSpeed + MinSpeed;
-            }
-        }
-
-        private bool IsValidAngle()
-		{
-			return transform.eulerAngles.z < HandRotationOrigin + HandRotationThreshhold
-						   && transform.eulerAngles.z > HandRotationOrigin - HandRotationThreshhold;
-		}
-
-        public float GetVerticalInput()
-        {
-            if (yVel > 0.01f){
-				float yAbs = yVel;
-
-                if (yAbs > MinSpeed)
-                {
-                    float result = yAbs / MaxSpeed;
-                    return Mathf.Min(result, 1f) * Mathf.Min(result, 1f);
-                }
-            }
-            return -1;
         }
     }
 
+    public void FixedUpdate()
+    {
+        if (IsValidAngle())
+        {
+            YVelocity -= BreakForceValidAngle;
+        }
+        else
+        {
+            YVelocity -= BreakForceWrongAngle;
+        }
+
+
+        if (YVelocity < 0f)
+        {
+            YVelocity = 0f;
+        }
+        else if (YVelocity > MaxSpeed + MinSpeed)
+        {
+            YVelocity = MaxSpeed + MinSpeed;
+        }
+    }
+
+    private bool IsValidAngle()
+    {
+        return transform.eulerAngles.z < HandRotationOrigin + HandRotationThreshhold
+               && transform.eulerAngles.z > HandRotationOrigin - HandRotationThreshhold;
+    }
+
+    public float GetVerticalInput()
+    {
+        if (YVelocity > 0.01f)
+        {
+            float yAbs = YVelocity;
+
+            if (yAbs > MinSpeed)
+            {
+                float result = yAbs / MaxSpeed;
+                return Mathf.Min(result, 1f) * Mathf.Min(result, 1f);
+            }
+        }
+        return -1;
+    }
 }
