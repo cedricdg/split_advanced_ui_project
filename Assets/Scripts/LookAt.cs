c065a7120e1007aa58106a0273038d980accb3e9
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-11-10 19:57:21 +0100

    Add witch

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtTest : MonoBehaviour
+{
+	public Transform Looking;
+	public Transform Target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Looking.LookAt(Target);
+	}
+}
---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+	public Transform Pivot;
+	public float Speed = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.RotateAround(Pivot.position, Vector3.up, Speed);
+	}
+}
