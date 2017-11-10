c908d72c9f789c3d1a4646934b98571a4f9050ed
Y0hy0h <Y0hy0h@users.noreply.github.com>
2017-11-10 15:37:06 +0100

    Add basic bullet firing

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public Transform SpawnPoint;
+    public Transform Container;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        var bullet = Instantiate(
+            BulletPrefab,
+            SpawnPoint.position,
+            SpawnPoint.rotation,
+            Container
+        );
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+    }
+}
